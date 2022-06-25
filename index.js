c122a193c841cc170a459c7c700ed09e7a4e500b
robozushi10 <robozushi10@gmail.com>
2022-06-26 03:54:31 +0900

    2022年  6月 26日 日曜日 03:54:31 JST

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 console.log("Hello, World!");
 const fname = core.getInput('log');
 console.log("fname => <${fname}>");
